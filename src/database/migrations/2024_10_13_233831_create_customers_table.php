c52a30afd9fd60dbe2599af7581773090c96f623
ykaho <h12.929.kaho@gmail.com>
2024-10-13 23:39:44 +0900

    10/13のコミット

---
+++ src/database/migrations/2024_10_13_233831_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('school', 100);
+            $table->string('tel', 11);
+            $table->string('email');
+            $table->string('manager');
+            $table->foreignID('manager_id')->constrained();
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
